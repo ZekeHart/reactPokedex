3a27acfd5b2eab3fd41056431d238208790cca8c
Zeke Hart <heirloch@gmail.com>
2019-08-28 17:59:28 -0400

    adds png with sprites, and cells for each pokemon in the list

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import './styles/PokeList.css'
+import PokeCell from './PokeCell'
+import { pokeClasses } from '../pokeClasses'
 
 const PokeList = () => {
+  const cells = pokeClasses.map(pokeClass => {
+    return (
+      <PokeCell
+        key={pokeClass.id}
+        pokeClass={pokeClass}
+      />
+    )
+  })
   return (
-    <section className='poke-list' />
+    <section className='poke-list'>
+      {cells}
+    </section>
   )
 }
 
