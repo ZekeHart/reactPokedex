a69079fad59bbe294ee0a80e67617a3873c8e500
Zeke Hart <heirloch@gmail.com>
2019-08-31 21:59:30 -0400

    adds weight to pkmn

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -2,13 +2,14 @@ import React from 'react'
 import './styles/DetailView.css'
 
 const DetailView = ({ pokemon }) => {
-  const { id, name, sprite, type } = pokemon
+  const { id, name, sprite, type, weight } = pokemon
   return (
     <section className='detail-view'>
       <img className='sprite-image' src={sprite} />
       <div className='data-wrapper'>
         <h1 className='data-name' >ID: {id} {name}</h1>
         <p className='data-char'>Type: {type}</p>
+        <p className='data-char'>Weight: {weight}</p>
       </div>
     </section>
   )
